10ea5c5fd3ddd10db737fd1b7352961be3eae61f
yolo <kuroikenshi@sina.com>
2019-05-06 18:03:07 +0800

    增加添加家长信息的入口，页面未写，记录进度

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -13,10 +13,18 @@
         <div class="center sliding stronger">{{navbarTitle}}</div>
 
         <div class="right">
+          <!-- TODO: 可以加一个自由定义的按钮，目前判断成本过高，放弃优化 -->
+          
           <!-- 发布班级动态 -->
           <a href="javascript: void(0);" class="tabbar-btn"
-              @click="momentAdd" v-show="showAddMoment">
+              @click="momentAdd" v-show="showMomentAdd">
             <i class="f7-icons">add</i>
+          </a>
+          
+          <!-- 添加家长信息 -->
+          <a href="javascript: void(0);" class="tabbar-btn link-color"
+              @click="parentAdd" v-show="showParentAdd">
+            添加
           </a>
         </div>
       </div>
@@ -54,21 +62,22 @@
         // msg: 'This is my component',
         navbarTitle: '----',
         backPath: '',
-        showAddMoment: false,
+        
+        showMomentAdd: false,
+        showParentAdd: false,
         showBackBtn: true,
       }
     },
     created () {
-      // 更新新增动态按钮的显示状态
-      this.updateShowAddMoment()
-      // 更新返回按钮的显示状态
-      this.updateShowBackBtn()
+      // 更新显示状态
+      this._updateShowState()
     },
     methods: {
       go: function (path) {
         // console.log('>>>', this.$router.history.current.path + '/' + path)
         this.$router.push(this.$router.history.current.path + '/' + path)
       },
+      
       goBack: function () {
         if (!this.backPath) {
           this.$router.back()
@@ -78,34 +87,36 @@
           this.$router.push(backPath)
         }
       },
+      
       // 是当前路径
       isCurrent: function (path) {
         return (this.$router.history.current.path.indexOf(path) > -1)
       },
+      
       // 是否显示navbar
       isNavShow: function () {
         // console.log('params>>>', this.$route)
         let hideNavPath = ['/user/userInfo']
         return (hideNavPath.indexOf(this.$router.history.current.path) === -1)
       },
-      // 是否显示navbar
+      
+      // 是否显示tabbar
       isTabShow: function () {
         // console.log('params>>>', this.$route)
         let hideTabPath = ['/class/momentAdd']
         return (hideTabPath.indexOf(this.$router.history.current.path) === -1)
       },
       
-      // 更新新增动态按钮的显示状态
-      updateShowAddMoment: function () {        
+      // 更新显示状态
+      _updateShowState: function () {
         let ut = window.uls.get('userInfo', 'userType')
-        // 是moments路径，且身份为老师或助教
-        console.log('>>> ut:', ut)
-        console.log(' -> 显示新增按钮:' + (new RegExp('\/class\/[^\/]*\/moments')).test(this.$router.history.current.path) + ' && ' + (ut === Global.userType.TEACHER))
-        this.showAddMoment = (new RegExp('\/class\/[^\/]*\/moments')).test(this.$router.history.current.path) && (ut === Global.userType.TEACHER)
-      },
-      
-      // 更新返回按钮的显示状态
-      updateShowBackBtn: function () {
+        // 显示发布动态按钮: 是moments路径，且身份为老师或助教
+        this.showMomentAdd = (new RegExp('\/class\/[^\/]*\/moments')).test(this.$router.history.current.path) && (ut === Global.userType.TEACHER)
+        
+        // 显示新增家长按钮: 是/user/parentInfoList路径
+        this.showParentAdd = (this.$router.history.current.path == '/user/parentInfoList')
+        
+        // 显示返回按钮: 非此路径
         this.showBackBtn = (this.$router.history.current.path !== '/class/classList')
       },
       
@@ -115,27 +126,35 @@
         for (let key in data) {
           this[key] = data[key]
         }
-        // 更新新增动态按钮的显示状态
-        this.updateShowAddMoment()
-        // 更新返回按钮的显示状态
-        this.updateShowBackBtn()
+        // 更新显示状态
+        this._updateShowState()
       },
-      // 进入班级动态
-      momentAdd () {    
+      
+      
+      // 进入添加动态页面
+      momentAdd () {
         this.$router.push('/class/momentAdd')
-      }
+      },
+      
+      // 进入添加家长信息页面
+      parentAdd () {
+        this.$router.push('/user/parentInfoAdd')
+      },
     }
   }
 </script>
 
 <style scoped>
-  .tabbar-btn {
-    color: #333!important;
+  .tabbar-btn .f7-icons {
+    color: #333;
   }
   .tabbar-btn i.f7-icons {
     font-size: 18px;
   }
   .right {
     min-width: 18px;
+  }
+  .tabbar-btn.link-color {
+    font-size: 16px;
   }
 </style>
