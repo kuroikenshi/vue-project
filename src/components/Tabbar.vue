4accf2561d522d1280685f0ee99de3aeda9d3eba
yolo <kuroikenshi@sina.com>
2019-07-31 17:20:45 +0800

    修改高亮逻辑：包含关键字 -> 以/关键字开头

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -14,13 +14,13 @@
 
         <div class="right">
           <!-- TODO: 可以加一个自由定义的按钮，目前判断成本过高，放弃优化 -->
-          
+
           <!-- 发布班级动态 -->
           <a href="javascript: void(0);" class="tabbar-btn"
               @click="momentAdd" v-show="showMomentAdd">
             <i class="f7-icons">add</i>
           </a>
-          
+
           <!-- 添加家长信息 -->
           <a href="javascript: void(0);" class="tabbar-btn link-color"
               @click="parentAdd" v-show="showParentAdd">
@@ -62,7 +62,7 @@
         // msg: 'This is my component',
         navbarTitle: '----',
         backPath: '',
-        
+
         showMomentAdd: false,
         showParentAdd: false,
         showBackBtn: true,
@@ -77,7 +77,7 @@
         // console.log('>>>', this.$router.history.current.path + '/' + path)
         this.$router.push(this.$router.history.current.path + '/' + path)
       },
-      
+
       goBack: function () {
         if (!this.backPath) {
           this.$router.back()
@@ -87,39 +87,40 @@
           this.$router.push(backPath)
         }
       },
-      
+
       // 是当前路径
       isCurrent: function (path) {
-        return (this.$router.history.current.path.indexOf(path) > -1)
+        // 根据开头路径区分
+        return (this.$router.history.current.path.indexOf('/' + path) == 0)
       },
-      
+
       // 是否显示navbar
       isNavShow: function () {
         // console.log('params>>>', this.$route)
         let hideNavPath = ['/user/userInfo']
         return (hideNavPath.indexOf(this.$router.history.current.path) === -1)
       },
-      
+
       // 是否显示tabbar
       isTabShow: function () {
         // console.log('params>>>', this.$route)
         let hideTabPath = ['/class/momentAdd']
         return (hideTabPath.indexOf(this.$router.history.current.path) === -1)
       },
-      
+
       // 更新显示状态
       _updateShowState: function () {
         let ut = window.uls.get('userInfo', 'userType')
         // 显示发布动态按钮: 是moments路径，且身份为老师或助教
         this.showMomentAdd = (new RegExp('\/class\/[^\/]*\/moments')).test(this.$router.history.current.path) && (ut === Global.userType.TEACHER)
-        
+
         // 显示新增家长按钮: 是/user/parentInfoList路径
         this.showParentAdd = (this.$router.history.current.path == '/user/parentInfoList')
-        
+
         // 显示返回按钮: 非此路径
         this.showBackBtn = (this.$router.history.current.path !== '/class/classList')
       },
-      
+
       // 更新一些值
       updateSomeValue: function(data) {
         // console.log('getNavbarTitle>>>>>>>>', this.navbarTitle, navbarTitle)
@@ -129,13 +130,13 @@
         // 更新显示状态
         this._updateShowState()
       },
-      
-      
+
+
       // 进入添加动态页面
       momentAdd () {
         this.$router.push('/class/momentAdd')
       },
-      
+
       // 进入添加家长信息页面
       parentAdd () {
         this.$router.push('/user/parentInfoAdd')
