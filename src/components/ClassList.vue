fcd47e8348dcf282a80f9f5f3a9559d4bb0568f4
yolo <kuroikenshi@sina.com>
2020-04-14 10:03:53 +0800

    解决因tomcat设置造成的resp状态小bug

--- src/components/ClassList.vue
+++ src/components/ClassList.vue
@@ -129,7 +129,7 @@
           this.$axios.post('/banji/moments/getNewsCount', postData).then(res => {
             console.log('getNewsCount>>>', res.data)
             this.classList[idx].count =  'n'
-            if (res.status === 200 && res.statusText === 'OK') {
+            if (res.status === 200) {
               console.log('    count:', res.data.data)
               console.log('    classItem.count:', classItem.count)
               classItem.count = res.data.data
